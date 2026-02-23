f6ed16631024d5b4c2c8623a5d1fd232a6739dee
Leonardo Armas <leo.armas.rs@gmail.com>
2026-02-23 17:27:31 -0600

    displayed debit accounts and credits

--- src/app/creditos/page.jsx
+++ src/app/creditos/page.jsx
@@ -2,10 +2,31 @@ export const metadata = {
     title: "Créditos"
 }
 
-export default function Creditos() {
+export default async function Creditos() {
+    const response = await fetch("http://localhost:3000/api/credits")
+    const data = await response.json()
+    console.log(data.message)
+    const cards = data.cards
     return (
         <main>
-            <h1>Créditos</h1>
+            <section id="creditos">
+                <h1>Créditos</h1>
+                <ul>
+                    {cards.map(card => <li key={card.id} className="card-preview">
+                        <article>
+                            <img src={`/products/credits/${card.image}.jpg`}
+                                alt="Card Image"
+                                className="card-image"
+                            />
+                            <h4><b>{card.creditName}</b></h4>
+                            <a href={`/creditos/${card.image}`}
+                                className="btn">
+                                ¡Me interesa!
+                            </a>
+                        </article>
+                    </li>)}
+                </ul>
+            </section>
         </main>
     )
 }
--- src/app/cuentas-de-debito/page.jsx
+++ src/app/cuentas-de-debito/page.jsx
@@ -2,10 +2,31 @@ export const metadata = {
     title: "Cuentas de débito"
 }
 
-export default function DebitAccounts() {
+export default async function DebitAccounts() {
+    const response = await fetch("http://localhost:3000/api/debit-accounts")
+    const data = await response.json()
+    console.log(data.message)
+    const cards = data.cards
     return (
         <main>
-            <h1>Cuentas de débito</h1>
+            <section id="debit-cards">
+                <h1>Cuentas de débito</h1>
+                <ul>
+                    {cards.map(card => <li key={card.id} className="card-preview">
+                        <article>
+                            <img src={`/products/debitAccounts/${card.image}.png`}
+                                alt="Card Image"
+                                className="card-image"
+                            />
+                            <h4><b>{card.accountName}</b></h4>
+                            <a href={`/cuentas-de-debito/${card.image}`}
+                                className="btn">
+                                ¡Me interesa!
+                            </a>
+                        </article>
+                    </li>)}
+                </ul>
+            </section>
         </main>
     )
 }
